3f535928c6776be8215b4e6f852d88f637221157
James <jamesarakaki@gmail.com>
2018-06-19 20:28:55 -0700

    added to static app, need to add ViewTodo component

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,56 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      description: '',
+      priority: '',
+      todos: []
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.viewTodo = this.viewTodo.bind(this);
+    this.addTodo = this.addTodo.bind(this);
+    this.deleteTodo = this.deleteTodo.bind(this);
+  }
+
   handleChange(e) {
     this.setState({ [e.target.name]: e.target.value });
   }
 
-  addTodo(e) {
-   e.preventDefault();
+  handleClick(e) {
+    var newArr = this.state.todos.slice();
+    if (this.state.description != '' && this.state.priority != 0) {
+      newArr.push({
+        description: this.state.description,
+        priority: this.state.priority
+      });
+      this.setState({ todos: newArr });
+    } else {
+      alert('Missing Description or Priority');
+      return null;
+    }
+  }
+
+  viewTodo() {
 
   }
 
-  removeTodo(e) {
-
+  addTodo(text, i) {
+    e.preventDefault();
+    let arr = this.state.todos.slice();
+    arr[i] = text;
+    this.setState({ todos: arr })
   }
 
-  editTodo() {
-    
+  deleteTodo(i) {
+    let arr = this.state.todos.slice();
+    arr.splice(i, 1);
+    this.setState({ todos: arr });
   }
-   
+
 
   render() {
     return (
@@ -36,18 +68,34 @@ class App extends Component {
               <div className='card-body'>
                 <div className='form-group'>
                   <label>I want to...</label>
-                  <textarea className='form-control input-lg' rows='5' id='add item' />
+                  <textarea
+                    id='add item'
+                    className='create-todo-text form-control'
+                    rows='5'
+                    value={this.state.description}
+                    onChange={this.handleChange}
+                  />
                   <label>How much of a priority is this?</label>
                   <div>
-                    <select className="form-control">
-                      <option></option>
-                      <option></option>
-                      <option></option>
+                    <select
+                      className='create-todo-priority form-control'
+                      value={this.state.priority}
+                      onChange={this.handleChange}
+                    >
+                      <option value={0} hidden>Select Priority</option>
+                      <option value={1}>Low Priority</option>
+                      <option value={2}>Medium Priority</option>
+                      <option value={3}>High Priority</option>
                     </select>
                   </div>
                 </div>
                 <div className='card-footer'>
-                  <button name='btn' type='submit' className='btn btn-primary form-control'>ADD</button>
+                  <button
+                    name='btn'
+                    type='submit'
+                    className='create-todo btn btn-primary form-control'
+                    onClick={this.handleClick}
+                  >ADD</button>
                 </div>
               </div>
             </div>
@@ -59,9 +107,20 @@ class App extends Component {
             <div className='card'>
               <h5 className='card-header'>View Todos</h5>
               <div className='card-body'>
-                <div>
-                  Welcome to the very simple todo app!
-                </div>
+                <ul className="list-group">
+                  {/*use ternary operator to display cover or list of items*/}
+                  {this.state.todos.length ? (
+                    this.state.todos.map(this.eachComment)
+                  ) : (
+                      <li className="list-group-item list-group-item-primary font-weight-bold">
+                        <span>Welcome to Very Simple Todo App!</span>
+                        <br />
+                        <a className="font-weight-normal">
+                          Get started now by adding a new todo on the left
+                    </a>
+                      </li>
+                    )}
+                </ul>
               </div>
             </div>
           </div>
